51bd290b86b589452c38a035648017bb5652f173
王林 <wb-wl511449@alibaba-inc.com>
2019-03-15 14:50:19 +0800

    审核list

--- pages/detailsMsg/detailsMsg.js
+++ pages/detailsMsg/detailsMsg.js
@@ -1,66 +1,45 @@
 // pages/detailsMsg/detailsMsg.js
+import {
+  feach
+} from '../../utils/util.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    id:'',
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad:function(options){
+    this.setData({
+      id: options.id
+    })
+    this.detailsDate(id);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  detailsDate(id){
+    let data = {
+      id,
+    }
+    feach('','get',data)
+    .then(res=>{
+      console.log(res)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  flooterbtn(){
+    let data = {
+       id:this.data.id,
+      status:'1',
+    }
+    feach('/admin/Driver/updateDriverStatus','get',data)
+    .then(res=>{
+      console.log(res)
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  flooternotBtn(){
+    let data = {
+      id: this.data.id,
+      status: '2',
+      marker:''
+    }
+    feach('/admin/Driver/updateDriverStatus', 'get', data)
+      .then(res => {
+        console.log(res)
+      })
   }
 })
