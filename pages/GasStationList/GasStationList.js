2a4b7fe9fd67f3f86abe989408c541a812d3e204
王林 <wb-wl511449@alibaba-inc.com>
2019-03-18 14:40:32 +0800

    a

--- pages/GasStationList/GasStationList.js
+++ pages/GasStationList/GasStationList.js
@@ -1,66 +1,39 @@
-// pages/GasStationList/GasStationList.js
+
+import {
+  feach
+} from '../../utils/util.js'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    list: [],
+    page: 1,
+    name: ''
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
+    this.getList()
   },
+  getList: function () {
+    let data = {
+      page: this.data.page,
+      name: this.data.name
+    }
+    feach('/admin/Carwash/getGasStationList', 'get', data)
+      .then(res => {
+        if (res.data.code === '0') {
+          this.setData({
+            list: [...this.data.list, ...res.data.data.list],
+            total_page: res.data.data.total_page,
+          })
+        }
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+      })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
   onReachBottom: function () {
+    if (this.data.page >= this.data.total_page) return;
+    this.setData({
+      page: ++this.data.page
 
+    })
+    // 上拉获取更多数据
+    this.getList()
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
